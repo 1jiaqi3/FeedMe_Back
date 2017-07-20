cdd305885979045a3e4949275d0f5f9fc661f98f
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-07-20 21:45:55 +0800

    Project Initiated

---
+++ src/main/java/com/jiaqi/SellmeBackApplication.java
@@ -0,0 +1,12 @@
+package com.jiaqi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellmeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellmeBackApplication.class, args);
+	}
+}
---
+++ src/test/java/com/jiaqi/LoggerTest.java
@@ -0,0 +1,11 @@
+package com.jiaqi;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by mac on 20/07/2017.
+ */
+public class LoggerTest {
+    private final Logger logger = LoggerFactory.getLogger(LoggerTest.class);
+}
