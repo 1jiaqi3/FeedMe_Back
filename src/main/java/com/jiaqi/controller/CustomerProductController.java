c109e401cd6b0e60665d567f1b5ab55d15f0b80b
1jiaqi3 <jiaqi.liu@vanderbilt.edu>
2017-07-22 22:22:55 +0800

    Query data from DB in controller, and sent to the fronEnd

--- src/main/java/com/jiaqi/controller/CustomerProductController.java
+++ src/main/java/com/jiaqi/controller/CustomerProductController.java
@@ -7,6 +7,7 @@ import com.jiaqi.dataobject.ProductCategory;
 import com.jiaqi.dataobject.ProductInfo;
 import com.jiaqi.service.ProductCategoryService;
 import com.jiaqi.service.ProductInfoService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,16 +31,39 @@ public class CustomerProductController {
 
     @GetMapping("/list")
     public ResultVo list() {
+        // Query all products that are up from our DB.
+        List<ProductInfo> productInfoList = productInfoService.findUpAll();
+
+        // Query Categories from DB
+        List<Integer> categoryTypeList = new ArrayList<>();
+        for (ProductInfo productInfo : productInfoList) {
+            categoryTypeList.add(productInfo.getCategoryType());
+        }
+        List<ProductCategory> productCategoryList = productCategoryService.findByCategoryTypeIn(categoryTypeList);
+
+        // data object build
+        List<ProductVO> productVOList = new ArrayList<>();
+        for (ProductCategory productCategory : productCategoryList) {
+            ProductVO productVO = new ProductVO();
+            productVO.setCategoryType(productCategory.getCategoryType());
+            productVO.setCategoryName(productCategory.getCategoryName());
+
+            List<ProductInfoVO> productInfoVOList = new ArrayList<>();
+            for (ProductInfo productInfo : productInfoList) {
+                if (productInfo.getCategoryType().equals(productCategory.getCategoryType())) {
+                    ProductInfoVO productInfoVO = new ProductInfoVO();
+                    BeanUtils.copyProperties(productInfo, productInfoVO);
+                    productInfoVOList.add(productInfoVO);
+                }
+            }
+            productVO.setProductInfoVOList(productInfoVOList);
+            productVOList.add(productVO);
+        }
 
         ResultVo resultVo = new ResultVo();
-
-        ProductVO productVO = new ProductVO();
-        ProductInfoVO productInfoVO = new ProductInfoVO();
-        productVO.setProductInfoVOList(Arrays.asList(productInfoVO));
-
         resultVo.setCode(0);
         resultVo.setMsg("Success");
-        resultVo.setData(Arrays.asList(productVO));
+        resultVo.setData(productVOList);
         return resultVo;
     }
 }
